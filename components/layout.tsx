c44fed924ecf29ba25156335501f6042a7453870
daniel s <iam@w-b.dev>
2021-05-03 01:42:03 -0300

    first release

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 import Head from "next/head";
 import Link from "next/link";
 
@@ -10,31 +10,56 @@ export default function Layout({
   isHome?: boolean;
 }) {
   return (
-    <div className="flex w-screen h-screen">
+    <div className="subpixel-antialiased flex flex-col container m-auto w-screen h-screen">
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Roboto:wght@700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Roboto&display=swap"
         />
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/site.webmanifest" />
       </Head>
       {isHome ? (
-        <header>Home nav menu here</header>
+        <header className="p-4 flex">
+          <nav className="flex-grow flex justify-between text-gray-400 text-sm font-serif cursor-pointer">
+            <Link href={"https://ca.linkedin.com/in/brasileiro"}>
+              <h2>LinkedIn</h2>
+            </Link>
+            <Link href={"https://github.com/w-b-dev"}>
+              <h2>Github</h2>
+            </Link>
+            <Link href={"https://mobile.twitter.com/brasileiro_ca"}>
+              <h2>🐦</h2>
+            </Link>
+            <Link href={"https://astra.datastax.com"}>
+              <h2 className="text-white">also️ working on...</h2>
+            </Link>
+          </nav>
+        </header>
       ) : (
         <>
-          <header>I am a header</header>
+          <header className="p-4 flex">
+            <nav className="flex-grow flex justify-between text-gray-700">
+              <Link href={"/"}>
+                <h2>🔙 home</h2>
+              </Link>
+            </nav>
+          </header>
           <hr />
         </>
       )}
-      <main>{children}</main>
-      {!isHome && (
-        <>
-          <hr />
-          <Link href={"/"}>Back home</Link>
-        </>
-      )}
+      <main className="container flex-grow">{children}</main>
+      <footer className="w-full text-right md:text-left">
+        <Link href={"https://unsplash.com/photos/pUGRSh9dS9M"}>
+          <span
+            title={"This amazing photo is from Willian Justen"}
+            className="text-gray-500 md:text-white text-xs md:text-md"
+          >
+            Photo credits
+          </span>
+        </Link>
+      </footer>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,86 @@
 import Head from "next/head";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import Layout from "../components/layout";
 // https://tailwindcss.com/docs/guides/nextjs
 export default function Home() {
+  const [url, setUrl] = useState("");
+  useEffect(() => {
+    setUrl(window?.location.hostname);
+  }, []);
   return (
     <Layout isHome>
       <Head>
         <title>Homepage</title>
       </Head>
-
-      <main className="container bg-gray-500 m-auto rounded-md shadow-md">
-        {/* TODO #1: Export as a shared component */}
-        <h1 className={"text-xl text-white p-6"}>
-          <span>Learn </span>
-          <Link href={"/posts/first-post"}>
-            <span className={"text-black bg-white font-medium"}>
-              with my posts
-            </span>
-          </Link>
-        </h1>
-      </main>
+      <section className="h-full flex flex-col md:flex-row md:justify-space-between md:flex-grow">
+        <main className="text-white m-auto">
+          {url && (
+            <div className="text-6xl md:text-9xl underline md:leading-loose">
+              {url}
+            </div>
+          )}
+          <div className="text-2xl md:text-6xl text-right italic leading-loose">
+            🐠 keep swimming
+          </div>
+        </main>
+        <aside
+          className={
+            "pt-1 pl-4 pr-4 pb-1 md:px-10 lg:p-10 max-h-80 md:max-h-96 overscroll-auto overflow-auto md:w-80 lg:w-96 m-3 md:m-auto mb-auto rounded-xl text-gray-300 bg-gray-700 bg-opacity-20 hover:bg-opacity-80 border border-black border-opacity-20 shadow-lg"
+          }
+        >
+          <h2 className="font-sans text-md underline leading-loose">
+            some recent posts:
+          </h2>
+          <ol className="md:list-decimal text-lg cursor-pointer">
+            <li className="hover:text-pink-400 hover:font-bold">
+              <Link
+                href={"https://reactjs.org/docs/optimizing-performance.html"}
+              >
+                <span>Optimize your React app</span>
+              </Link>
+            </li>
+            <li className="hover:text-pink-400 hover:font-bold">
+              <Link href={"https://www.robinwieruch.de/react-hooks-fetch-data"}>
+                <span>An extensive guide on how to fetch data with Hooks</span>
+              </Link>
+            </li>
+            <li className="hover:text-pink-400 hover:font-bold">
+              <Link
+                href={
+                  "https://epicreact.dev/one-react-mistake-thats-slowing-you-down"
+                }
+              >
+                <span>Composition with React</span>
+              </Link>
+            </li>
+            <li className="hover:text-pink-400 hover:font-bold">
+              <Link href={"https://nextjs.org/blog/incremental-adoption"}>
+                <span>How to incrementally start using Next.js</span>
+              </Link>
+            </li>
+            <li className="hover:text-pink-400 hover:font-bold">
+              <Link href={"https://tailwindcss.com/docs/cursor"}>
+                <span>📌Tailwind docs</span>
+              </Link>
+            </li>
+            <li className="hover:text-pink-400 hover:font-bold">
+              <Link href={"https://nodejs.org/dist/latest/docs/api/"}>
+                <span>Node.js official docs</span>
+              </Link>
+            </li>
+            <li className="hover:text-pink-400 hover:font-bold">
+              <Link
+                href={
+                  "https://docs.microsoft.com/en-us/azure/cosmos-db/create-cassandra-nodejs"
+                }
+              >
+                <span>Cassandra with Node (Azure)</span>
+              </Link>
+            </li>
+          </ol>
+        </aside>
+      </section>
     </Layout>
   );
 }
