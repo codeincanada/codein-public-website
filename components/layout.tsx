4d9b6eef7cb02172c00fa6c6272500b42b8884c2
Daniel Santos <iam@w-b.dev>
2021-05-06 01:57:56 -0300

    udpate `Link` to `a` for external links

--- components/layout.tsx
+++ components/layout.tsx
@@ -15,6 +15,7 @@ export default function Layout({
   return (
     <div className="subpixel-antialiased  flex flex-col container m-auto w-screen h-screen">
       <Head>
+        <meta name="referrer" content="origin" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           rel="stylesheet"
@@ -27,18 +28,30 @@ export default function Layout({
       {isHome ? (
         <header className="p-4 flex">
           <nav className="flex-grow flex justify-between text-gray-400 text-xs cursor-pointer">
-            <Link href={"https://ca.linkedin.com/in/brasileiro"}>
+            <a href={"https://ca.linkedin.com/in/brasileiro"}>
               <h2>LinkedIn</h2>
-            </Link>
-            <Link href={"https://github.com/w-b-dev"}>
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={"https://github.com/w-b-dev"}
+            >
               <h2>Github</h2>
-            </Link>
-            <Link href={"https://mobile.twitter.com/brasileiro_ca"}>
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={"https://mobile.twitter.com/brasileiro_ca"}
+            >
               <h2>🐦</h2>
-            </Link>
-            <Link href={"https://astra.datastax.com"}>
+            </a>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href={"https://astra.datastax.com"}
+            >
               <h2>🔙</h2>
-            </Link>
+            </a>
           </nav>
         </header>
       ) : (
@@ -60,14 +73,18 @@ export default function Layout({
             <span className="">{url}</span>
           </Link>
         )}
-        <Link href={"https://unsplash.com/photos/pUGRSh9dS9M"}>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href={"https://unsplash.com/photos/pUGRSh9dS9M"}
+        >
           <span
             title={"This amazing photo is from Willian Justen"}
             className=""
           >
             Photo credits
           </span>
-        </Link>
+        </a>
       </footer>
     </div>
   );
