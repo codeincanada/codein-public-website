9cd44d8d9d595ea0b76741c1b5d346778eefccad
Daniel Santos <iam@w-b.dev>
2021-05-10 17:03:24 -0300

    update `target="_blank"`

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,12 +17,20 @@ export default function Home() {
           {/* <h2 className="underline leading-loose">suggested posts:</h2>*/}
           <ol className="md:list-decimal list-inside md:text-xl cursor-pointer space-y-4 divide-y divide-pink-400 divide-dotted leading-loose">
             <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
-              <a href={"https://reactjs.org/docs/optimizing-performance.html"}>
+              <a
+                href={"https://reactjs.org/docs/optimizing-performance.html"}
+                target="_blank"
+                rel="noreferrer"
+              >
                 <span>Optimize your React app</span>
               </a>
             </li>
             <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
-              <a href={"https://www.robinwieruch.de/react-hooks-fetch-data"}>
+              <a
+                href={"https://www.robinwieruch.de/react-hooks-fetch-data"}
+                target="_blank"
+                rel="noreferrer"
+              >
                 <span>An extensive guide on how to fetch data with Hooks</span>
               </a>
             </li>
@@ -31,22 +39,36 @@ export default function Home() {
                 href={
                   "https://epicreact.dev/one-react-mistake-thats-slowing-you-down"
                 }
+                target="_blank"
+                rel="noreferrer"
               >
                 <span>Composition with React</span>
               </a>
             </li>
             <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
-              <a href={"https://nextjs.org/blog/incremental-adoption"}>
+              <a
+                href={"https://nextjs.org/blog/incremental-adoption"}
+                target="_blank"
+                rel="noreferrer"
+              >
                 <span>How to incrementally start using Next.js</span>
               </a>
             </li>
             <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
-              <a href={"https://tailwindcss.com/docs/cursor"}>
+              <a
+                href={"https://tailwindcss.com/docs/cursor"}
+                target="_blank"
+                rel="noreferrer"
+              >
                 <span>Tailwind docs 📌</span>
               </a>
             </li>
             <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
-              <a href={"https://nodejs.org/dist/latest/docs/api/"}>
+              <a
+                href={"https://nodejs.org/dist/latest/docs/api/"}
+                target="_blank"
+                rel="noreferrer"
+              >
                 <span>Node.js official docs</span>
               </a>
             </li>
@@ -55,6 +77,8 @@ export default function Home() {
                 href={
                   "https://docs.microsoft.com/en-us/azure/cosmos-db/create-cassandra-nodejs"
                 }
+                target="_blank"
+                rel="noreferrer"
               >
                 <span>Cassandra with Node (Azure)</span>
               </a>
