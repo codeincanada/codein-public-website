3a7dfd234631a7413822c60f716acfd475c4ad8e
Daniel Santos <iam@w-b.dev>
2021-05-10 17:00:30 -0300

    add support for Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,23 @@
 import { AppProps } from "next/app";
 import "../styles/global.css";
 import "tailwindcss/tailwind.css";
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 /* Global imports should be here -- not in the Layout partial */
+// eslint-disable-next-line no-undef
+const isProduction = process.env.NODE_ENV === "production";
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      if (isProduction) gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />;
 }
