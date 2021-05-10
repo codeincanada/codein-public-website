74d458b2d8de9f424d213d025a04e98ce9873852
Daniel Santos <iam@w-b.dev>
2021-05-10 16:26:55 -0300

    update links to open in a new tab

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Link from "next/link";
 import Layout from "../components/layout";
 // https://tailwindcss.com/docs/guides/nextjs
 export default function Home() {
@@ -8,8 +7,62 @@ export default function Home() {
       <Head>
         <title>Homepage</title>
       </Head>
-      <section className="flex flex-col flex-grow p-1 md:p-10">
-        {/* <main className="text-white">*/}
+      <section className="flex flex-grow border border-black border-opacity-20 shadow-lg">
+        <aside
+          id={"article-list"}
+          className={
+            "m-auto transition delay-150 duration-300 ease-in-out p-10 md:rounded-xl text-gray-300 bg-opacity-10 hover:bg-opacity-50 bg-black hover:bg-black"
+          }
+        >
+          {/* <h2 className="underline leading-loose">suggested posts:</h2>*/}
+          <ol className="md:list-decimal list-inside md:text-xl cursor-pointer space-y-4 divide-y divide-pink-400 divide-dotted leading-loose">
+            <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
+              <a href={"https://reactjs.org/docs/optimizing-performance.html"}>
+                <span>Optimize your React app</span>
+              </a>
+            </li>
+            <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
+              <a href={"https://www.robinwieruch.de/react-hooks-fetch-data"}>
+                <span>An extensive guide on how to fetch data with Hooks</span>
+              </a>
+            </li>
+            <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
+              <a
+                href={
+                  "https://epicreact.dev/one-react-mistake-thats-slowing-you-down"
+                }
+              >
+                <span>Composition with React</span>
+              </a>
+            </li>
+            <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
+              <a href={"https://nextjs.org/blog/incremental-adoption"}>
+                <span>How to incrementally start using Next.js</span>
+              </a>
+            </li>
+            <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
+              <a href={"https://tailwindcss.com/docs/cursor"}>
+                <span>Tailwind docs 📌</span>
+              </a>
+            </li>
+            <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
+              <a href={"https://nodejs.org/dist/latest/docs/api/"}>
+                <span>Node.js official docs</span>
+              </a>
+            </li>
+            <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
+              <a
+                href={
+                  "https://docs.microsoft.com/en-us/azure/cosmos-db/create-cassandra-nodejs"
+                }
+              >
+                <span>Cassandra with Node (Azure)</span>
+              </a>
+            </li>
+          </ol>
+        </aside>
+        {/* MAIN READING PANEL*/}
+        {/* <main className="flex-1 bg-white bg-opacity-20">*/}
         {/*  <div*/}
         {/*    title="keep swimming"*/}
         {/*    className="text-6xl leading-loose text-center w-full"*/}
@@ -17,60 +70,6 @@ export default function Home() {
         {/*    🐠*/}
         {/*  </div>*/}
         {/* </main>*/}
-        <aside
-          className={
-            "transition delay-150 duration-300 ease-in-out pt-1 pl-4 pr-4 pb-1 md:px-10 lg:p-10 m-auto md:rounded-xl text-gray-300 bg-opacity-10 hover:bg-opacity-50 bg-black hover:bg-black border border-black border-opacity-20 shadow-lg"
-          }
-        >
-          {/* <h2 className="underline leading-loose">suggested posts:</h2>*/}
-          <ol className="md:list-decimal md:text-xl cursor-pointer space-y-4 divide-y divide-pink-400 divide-dotted leading-loose">
-            <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
-              <Link
-                href={"https://reactjs.org/docs/optimizing-performance.html"}
-              >
-                <span>Optimize your React app</span>
-              </Link>
-            </li>
-            <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
-              <Link href={"https://www.robinwieruch.de/react-hooks-fetch-data"}>
-                <span>An extensive guide on how to fetch data with Hooks</span>
-              </Link>
-            </li>
-            <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
-              <Link
-                href={
-                  "https://epicreact.dev/one-react-mistake-thats-slowing-you-down"
-                }
-              >
-                <span>Composition with React</span>
-              </Link>
-            </li>
-            <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
-              <Link href={"https://nextjs.org/blog/incremental-adoption"}>
-                <span>How to incrementally start using Next.js</span>
-              </Link>
-            </li>
-            <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
-              <Link href={"https://tailwindcss.com/docs/cursor"}>
-                <span>Tailwind docs 📌</span>
-              </Link>
-            </li>
-            <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
-              <Link href={"https://nodejs.org/dist/latest/docs/api/"}>
-                <span>Node.js official docs</span>
-              </Link>
-            </li>
-            <li className="hover:text-pink-400 filter drop-shadow transition delay-50 duration-200 ease-in-out">
-              <Link
-                href={
-                  "https://docs.microsoft.com/en-us/azure/cosmos-db/create-cassandra-nodejs"
-                }
-              >
-                <span>Cassandra with Node (Azure)</span>
-              </Link>
-            </li>
-          </ol>
-        </aside>
       </section>
     </Layout>
   );
