d6bcfdf671637230696b32911bad92e43ab39f3c
daniel s <iam@w-b.dev>
2021-05-13 00:29:46 -0300

    updates based on next.js tutorial
    reference:
    - https://nextjs.org/learn/basics/assets-metadata-css/styling-tips

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,19 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    "tailwindcss",
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        autoprefixer: {
+          flexbox: "no-2009",
+        },
+        stage: 3,
+        features: {
+          "custom-properties": false,
+        },
+      },
+    ],
+  ],
 };
