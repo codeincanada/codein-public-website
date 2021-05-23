67559f68a5a0b3796b4c4a7a17b987a56ffc98d7
daniel s <iam@w-b.dev>
2021-05-23 04:31:34 -0300

    update article list style

--- components/listItem.tsx
+++ components/listItem.tsx
@@ -9,11 +9,11 @@ export default function ListItemCA({
 }) {
   return isHighlighted ? (
     /* filter drop-shadow*/
-    <li className="mb-4 pl-2 p-2 border-b-4 border-solid border-pink-500 hover:border-white text-2xl md:text-3xl text-white hover:text-pink-500 transition delay-50 duration-200 ease-in-out">
+    <li className="mb-4 text-2xl md:text-3xl text-white hover:text-pink-500 border-b-2 border-solid border-pink-500 hover:border-white transition delay-50 duration-200 ease-in-out">
       {children}
     </li>
   ) : (
-    <li className="mx-4 py-2 px-4 text-xl md:text-2xl text-gray-400 hover:text-pink-400 border-b-2 border-solid border-pink-500 border-opacity-25">
+    <li className="mx-2 text-xl md:text-2xl text-white hover:text-pink-500 border-b-2 border-solid border-pink-500 border-opacity-25 transition delay-50 duration-200 ease-in-out">
       {children}
     </li>
   );
