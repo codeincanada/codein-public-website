4e14d8f487e8be087a0345031b7c107fdc3c5207
Daniel Santos <iam@w-b.dev>
2021-06-18 17:56:28 -0300

    try purge tailwind again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 // eslint-disable-next-line no-undef
+const remark = require("remark");
+// eslint-disable-next-line no-undef
 module.exports = {
   purge: ["./pages/**/*.{tsx}", "./components/**/*.{tsx}"],
+  transform: {
+    tsx: (content) => {
+      return remark().process(content);
+    },
+  },
   // darkMode: "media", // or 'class'
   theme: {
     fontFamily: {
